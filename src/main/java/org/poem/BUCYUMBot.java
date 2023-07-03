540a9d472699b3d74e666b869fbe59d1834c8a9a
yhh <hh312@tom.com>
2023-07-03 09:49:44 +0800

    成功跑起来了！

--- src/main/java/org/poem/BUCYUMBot.java
+++ src/main/java/org/poem/BUCYUMBot.java
@@ -8,40 +8,56 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class BUCYUMBot extends TelegramLongPollingBot {
-
-    //填你自己的token和username
-    private final String token = "5913104395:AAGf5AiCXY649u64YMW5ch40at5OUF4UUWA";
-    private final String username = "BUCYUMBot";
-
-    public BUCYUMBot() {
+    DatagramSocket Socket = new DatagramSocket(2365);
+    public BUCYUMBot() throws SocketException {
         this(new DefaultBotOptions());
     }
 
-    public BUCYUMBot(DefaultBotOptions options) {
+    public BUCYUMBot(DefaultBotOptions options) throws SocketException {
         super(options);
     }
 
     @Override
     public String getBotToken() {
-        return this.token;
+        //填你自己的token和username
+        return "5913104395:AAGf5AiCXY649u64YMW5ch40at5OUF4UUWA";
     }
 
     @Override
     public String getBotUsername() {
-        return this.username;
+        return "BUCYUMBot";
+    }
+
+    public void sendToSer(String message) throws IOException {
+        DatagramPacket datagramPacket = new DatagramPacket(message.getBytes(),
+                message.getBytes().length, InetAddress.getByName("127.0.0.1"), 2366);
+
+        Socket.send(datagramPacket);
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
-            Message message = update.getMessage();
-
-            Long chatId = message.getChatId();
-
-            String text = message.getText();
-
-            sendMsg("不", chatId);
+            try {
+                Message message = update.getMessage();
+                Long chatId = message.getChatId();
+                String text = message.getText();
+                String[] ifcmd = text.split(" ");
+                if (ifcmd[0].equals("绑定基岩版")){
+                    if (ifcmd[1].matches("[A-Za-z0-9_]*")) {
+                        sendMsg("绑定成功！",chatId);
+                    }
+                }
+                String formatmessage = "["+message.getFrom().getUserName()+"] " + text;
+                sendToSer(formatmessage);
+            } catch (IOException ignored) {}
         }
     }
 
--- src/main/java/org/poem/Main.java
+++ src/main/java/org/poem/Main.java
@@ -5,14 +5,23 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.net.SocketException;
+
 public class Main {
     public static void main(String[] args) {
-        DefaultBotSession defaultBotSession = new DefaultBotSession();
         try {
+            DefaultBotOptions botOptions = new DefaultBotOptions();
+            botOptions.setProxyHost("127.0.0.1");
+            botOptions.setProxyPort(10810);
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
+
+            DefaultBotSession defaultBotSession = new DefaultBotSession();
+            defaultBotSession.setOptions(botOptions);
+
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(defaultBotSession.getClass());
-            BUCYUMBot bot2 = new BUCYUMBot();
+            BUCYUMBot bot2 = new BUCYUMBot(botOptions);
             telegramBotsApi.registerBot(bot2);
-        } catch (TelegramApiException e) {
+        } catch (TelegramApiException | SocketException e) {
             e.printStackTrace();
         }
     }
